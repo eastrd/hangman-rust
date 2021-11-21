8b40d5c3714bcbe979df66f244b51cdad1272311
eastrd <e0t3rx@gmail.com>
2021-11-21 17:23:42 +1100

    Add loop & Fix win condition bug

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use rand::prelude::IteratorRandom;
 use std::{
     fs,
     io::{stdin, stdout, Read, Write},
-    ops::RangeBounds,
 };
 
 fn get_random_word() -> String {
@@ -26,55 +25,58 @@ fn get_random_word() -> String {
 }
 
 fn main() {
-    let word = get_random_word();
-    let mut good_chars: Vec<char> = Vec::new();
-    let mut bad_chars: Vec<char> = Vec::new();
-    let mut life: i8 = 10;
+    loop {
+        let word = get_random_word();
+        let mut good_chars: Vec<char> = Vec::new();
+        let mut bad_chars: Vec<char> = Vec::new();
+        let mut life: i8 = 10;
 
-    // End loop either ran out of guesses OR guessed everything
-    while life > 0
-        || word
-            .chars()
-            .filter(|c| !good_chars.contains(c))
-            .collect::<Vec<char>>()
-            .len()
-            == 0
-    {
-        // render
-        display_gap();
-        display_word(&word, &good_chars);
-        display_chars(&good_chars, &bad_chars);
-        display_life(life);
+        // End loop either ran out of guesses OR guessed everything
+        while life > 0
+            && word
+                .chars()
+                .filter(|c| !good_chars.contains(c))
+                .collect::<Vec<char>>()
+                .len()
+                != 0
+        {
+            // render
+            display_gap();
+            display_word(&word, &good_chars);
+            display_chars(&good_chars, &bad_chars);
+            display_life(life);
 
-        // read first character of user input
-        let mut buf = String::new();
-        print!("Your Guess >> ");
-        stdout().flush().unwrap();
+            // read first character of user input
+            let mut buf = String::new();
+            print!("Your Guess >> ");
+            stdout().flush().unwrap();
 
-        stdin()
-            .read_line(&mut buf)
-            .expect("error reading user input");
-        let guess = buf.chars().next().unwrap();
+            stdin()
+                .read_line(&mut buf)
+                .expect("error reading user input");
+            let guess = buf.chars().next().unwrap();
 
-        // If guess has already been used, retry input
-        if good_chars.contains(&guess) || bad_chars.contains(&guess) {
-            println!("[!] You have already guessed '{}' before", guess);
-            continue;
+            // If guess has already been used, retry input
+            if good_chars.contains(&guess) || bad_chars.contains(&guess) {
+                println!("[!] You have already guessed '{}' before", guess);
+                continue;
+            }
+
+            if word.contains(&guess.to_string()) {
+                good_chars.push(guess);
+            } else {
+                bad_chars.push(guess);
+                println!("[!] '{}' is NOT in the word\n", guess);
+                life -= 1;
+            }
         }
 
-        if word.contains(&guess.to_string()) {
-            good_chars.push(guess);
+        if life == 0 {
+            println!("[!!!!!] Game over, the word is >>>'{}'<<<", word);
         } else {
-            bad_chars.push(guess);
-            println!("[!] '{}' is NOT in the word\n", guess);
-            life -= 1;
+            println!("[!!!!!] WOW, Unbelievable, HOW TF DID YOU GUESS THAT?!");
         }
-    }
-
-    if life == 0 {
-        println!("[!!!!!] Game over, the word is >>>'{}'<<<", word);
-    } else {
-        println!("[!!!!!] WOW, Unbelievable, HOW TF DID YOU GUESS THAT?!");
+        println!("\n\nLet's play another round!");
     }
 }
 
